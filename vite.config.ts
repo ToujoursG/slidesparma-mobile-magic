9dc76a38a2be519131595d5a51bc841a770cba30
ToujoursG <gustavosiegrist.m@gmail.com>
2025-06-29 17:12:05 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/slidesparma-mobile-magic/",
+  base: "/slidesparma-mobile-magic/"
   server: {
     host: "::",
     port: 8080,
